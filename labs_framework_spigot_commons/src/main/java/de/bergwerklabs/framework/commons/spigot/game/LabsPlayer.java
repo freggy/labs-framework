3546351727378696b214060121877c3c1040e5c2
freggy <freggy@bergwerklabs.de>
2017-07-02 13:32:05 +0200

    Added freezing functionality. This is a better alternative to canceling the PlayerMoveEvent

--- labs_framework_spigot_commons/src/main/java/de/bergwerklabs/framework/commons/spigot/game/LabsPlayer.java
+++ labs_framework_spigot_commons/src/main/java/de/bergwerklabs/framework/commons/spigot/game/LabsPlayer.java
@@ -1,5 +1,9 @@
 package de.bergwerklabs.framework.commons.spigot.game;
 
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
 /**
  * Created by Yannic Rieger on 01.05.2017.
  * <p>  </p>
@@ -8,7 +12,27 @@ package de.bergwerklabs.framework.commons.spigot.game;
  */
 public class LabsPlayer {
 
+    /**
+     * Freezes the player completely.
+     *
+     * @param player Player to freeze.
+     * @param ticks ticks the player shouldn't be able to jump.
+     */
+    public static void freeze(Player player, int ticks) {
+        player.setWalkSpeed(0);
+        PotionEffect effect = new PotionEffect(PotionEffectType.JUMP, ticks, 128, false, false);
+        effect.apply(player);
+    }
 
-
-
+    /**
+     * Unfreezes the player.
+     *
+     * @param player Player to unfreeze.
+     */
+    public static void unfreeze(Player player) {
+        if (player.hasPotionEffect(PotionEffectType.JUMP)) {
+            player.removePotionEffect(PotionEffectType.JUMP);
+        }
+        player.setWalkSpeed(0.2F);
+    }
 }
