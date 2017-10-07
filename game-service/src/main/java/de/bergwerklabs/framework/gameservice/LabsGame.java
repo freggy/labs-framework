7e5774bd7b770865a19de35fd1c6fc5270afb34b
freggy <riegeryannic@gmail.com>
2017-10-07 22:36:50 +0200

    changed module names and changed pom

---
+++ game-service/src/main/java/de/bergwerklabs/framework/gameservice/LabsGame.java
@@ -0,0 +1,37 @@
+package de.bergwerklabs.framework.gameservice;
+
+import de.bergwerklabs.commons.spigot.chat.messenger.PluginMessenger;
+import de.bergwerklabs.framework.gameservice.listener.LabsListener;
+import org.bukkit.Bukkit;
+
+/**
+ * Created by Yannic Rieger on 07.07.2017.
+ * <p>
+ *
+ * @author Yannic Rieger
+ */
+public abstract class LabsGame<T extends LabsPlayer> {
+
+    /**
+     * Gets the {@link PlayerRegistry} for this game.
+     */
+    public PlayerRegistry<T> getPlayerRegistry() { return this.playerRegistry; }
+
+    protected PlayerRegistry<T> playerRegistry = new PlayerRegistry<>();
+    protected PluginMessenger messenger;
+
+    public LabsGame(String name) {
+        Bukkit.getPluginManager().registerEvents(new LabsListener<>(playerRegistry, GameService.getInstance().getServiceConfig()), GameService.getInstance());
+        this.messenger = new PluginMessenger(name);
+    }
+
+    /**
+     * Starts the game.
+     */
+    public abstract void start();
+
+    /**
+     * Stops the game.
+     */
+    public abstract void stop();
+}
