ba5aaeb2a085f891338850235d8a5fedde50cbbe
Benedikt Bw2801 <wuelleronline@gmail.com>
2017-10-11 19:04:44 +0200

    Remove sql driver dependency; Fix particle util

--- spigot-commons/src/main/java/de/bergwerklabs/framework/commons/spigot/particle/ParticleUtil.java
+++ spigot-commons/src/main/java/de/bergwerklabs/framework/commons/spigot/particle/ParticleUtil.java
@@ -1,11 +1,14 @@
 package de.bergwerklabs.framework.commons.spigot.particle;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.EnumWrappers;
 import de.bergwerklabs.framework.commons.math.vector.Vector3F;
-import de.bergwerklabs.framework.commons.spigot.nms.packet.v1_8.WrapperPlayServerWorldParticles;
+import de.bergwerklabs.framework.commons.spigot.SpigotCommons;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 
 public class ParticleUtil {
@@ -23,7 +26,10 @@ public class ParticleUtil {
     }
 
     public static void spawnParticle(Player player, EnumWrappers.Particle particle, int amount, Vector3F position, Vector3F maxOffset, float data, boolean longDistance) {
-        createPacket(particle, amount, position, maxOffset, data, longDistance).sendPacket(player);
+        PacketContainer container = createPacket(particle, amount, position, maxOffset, data, longDistance);
+        try {
+            SpigotCommons.getInstance().getProtocolManager().sendServerPacket(player, container);
+        } catch (InvocationTargetException ignored) {}
     }
 
     public static void broadcastParticle(EnumWrappers.Particle particle, int amount, Vector3F position) {
@@ -39,8 +45,12 @@ public class ParticleUtil {
     }
 
     public static void broadcastParticle(EnumWrappers.Particle particle, int amount, Vector3F position, Vector3F maxOffset, float data, boolean longDistance) {
-        WrapperPlayServerWorldParticles packet = createPacket(particle, amount, position, maxOffset, data, longDistance);
-        Bukkit.getServer().getOnlinePlayers().forEach(packet::sendPacket);
+        PacketContainer container = createPacket(particle, amount, position, maxOffset, data, longDistance);
+        Bukkit.getServer().getOnlinePlayers().forEach(player -> {
+            try {
+                SpigotCommons.getInstance().getProtocolManager().sendServerPacket(player, container);
+            } catch (InvocationTargetException ignored) {}
+        });
     }
 
     public static void broadcastParticle(Collection<Player> players, EnumWrappers.Particle particle, int amount, Vector3F position) {
@@ -56,22 +66,26 @@ public class ParticleUtil {
     }
 
     public static void broadcastParticle(Collection<Player> players, EnumWrappers.Particle particle, int amount, Vector3F position, Vector3F maxOffset, float data, boolean longDistance) {
-        WrapperPlayServerWorldParticles packet = createPacket(particle, amount, position, maxOffset, data, longDistance);
-        players.forEach(packet::sendPacket);
+        PacketContainer container = createPacket(particle, amount, position, maxOffset, data, longDistance);
+        players.forEach(player -> {
+            try {
+                SpigotCommons.getInstance().getProtocolManager().sendServerPacket(player, container);
+            } catch (InvocationTargetException ignored) {}
+        });
     }
 
-    private static WrapperPlayServerWorldParticles createPacket(EnumWrappers.Particle particle, int amount, Vector3F position, Vector3F maxOffset, float data, boolean longDistance) {
-        WrapperPlayServerWorldParticles packet = new WrapperPlayServerWorldParticles();
-        packet.setParticleType(particle);
-        packet.setX(position.getX());
-        packet.setX(position.getY());
-        packet.setX(position.getZ());
-        packet.setOffsetX(maxOffset.getX());
-        packet.setOffsetY(maxOffset.getY());
-        packet.setOffsetZ(maxOffset.getZ());
-        packet.setParticleData(data);
-        packet.setLongDistance(longDistance);
-        packet.setNumberOfParticles(amount);
-        return packet;
+    private static PacketContainer createPacket(EnumWrappers.Particle particle, int amount, Vector3F position, Vector3F maxOffset, float data, boolean longDistance) {
+        PacketContainer container = SpigotCommons.getInstance().getProtocolManager().createPacket(PacketType.Play.Server.WORLD_PARTICLES);
+        container.getParticles().write(0, particle);
+        container.getFloat().write(0, position.getX()); // X
+        container.getFloat().write(1, position.getY()); // Y
+        container.getFloat().write(2, position.getZ()); // Z
+        container.getFloat().write(3, maxOffset.getX()); // OFFSET X
+        container.getFloat().write(4, maxOffset.getY()); // OFFSET Y
+        container.getFloat().write(5, maxOffset.getZ()); // OFFSET Z
+        container.getFloat().write(6, data); // DATA
+        container.getIntegers().write(0, amount); // COUNT
+        container.getBooleans().write(0, longDistance); // LONG DISTANCE
+        return container ;
     }
 }
