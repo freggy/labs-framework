7abff18f3bc3bbeba21b83a7d692ab6a32ebeaf1
freggy <riegeryannic@gmail.com>
2017-10-25 19:58:13 +0200

    Added method for receiving the chat color by the given color code

--- chat-commons/src/main/java/de/bergwerklabs/commons/spigot/chat/ChatCommons.java
+++ chat-commons/src/main/java/de/bergwerklabs/commons/spigot/chat/ChatCommons.java
@@ -1,7 +1,10 @@
 package de.bergwerklabs.commons.spigot.chat;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Optional;
 
 /**
  * Created by Yannic Rieger on 25.09.2017.
@@ -12,4 +15,20 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class ChatCommons extends JavaPlugin implements Listener {
 
+    /**
+     * Gets the fitting {@link ChatColor} by the given color code. Valid color codes are: e.g {@code §e} or {@code &e}
+     *
+     * @param colorCode can be {@code §e} or {@code &e} where {@code e} is the char specifying the color.
+     * @return          optional that can contains the {@link ChatColor}.
+     */
+    public static Optional<ChatColor> chatColorFromColorCode(String colorCode) {
+        for (ChatColor color : ChatColor.values()) {
+            String colorString1 = ChatColor.COLOR_CHAR + String.valueOf(color.getChar()); // e.g §e
+            String colorString2 = "&" + String.valueOf(color.getChar()); // e.g &e
+            if (colorString1.equalsIgnoreCase(colorCode) || colorString2.equalsIgnoreCase(colorCode)) {
+                return Optional.of(color);
+            }
+        }
+        return Optional.empty();
+    }
 }
