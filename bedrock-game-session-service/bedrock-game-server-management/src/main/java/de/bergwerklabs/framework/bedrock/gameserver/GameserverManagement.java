a9c34a5b79f1fa89556a56b253cc66a017d6b45c
freggy <riegeryannic@gmail.com>
2017-11-23 21:13:04 +0100

    Started work on bedrock game server management

---
+++ bedrock-game-session-service/bedrock-game-server-management/src/main/java/de/bergwerklabs/framework/bedrock/gameserver/GameserverManagement.java
@@ -0,0 +1,18 @@
+package de.bergwerklabs.framework.bedrock.gameserver;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Yannic Rieger on 23.11.2017.
+ * <p>
+ *
+ * @author Yannic Rieger
+ */
+public class GameserverManagement extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO register listeners
+    }
+}
