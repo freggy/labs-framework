29457b9d281dba21760c31ea82f5c1aa7ff0a27f
Benedikt Bw2801 <wuelleronline@gmail.com>
2017-12-05 23:20:20 +0100

    Allow float vector creation from location

--- spigot-commons/src/main/java/de/bergwerklabs/framework/commons/spigot/math/VectorUtil.java
+++ spigot-commons/src/main/java/de/bergwerklabs/framework/commons/spigot/math/VectorUtil.java
@@ -1,6 +1,7 @@
 package de.bergwerklabs.framework.commons.spigot.math;
 
 import de.bergwerklabs.framework.commons.math.vector.Vector3D;
+import de.bergwerklabs.framework.commons.math.vector.Vector3F;
 import org.bukkit.Location;
 import org.bukkit.World;
 
@@ -10,11 +11,23 @@ public class VectorUtil {
         return new Vector3D(location.getX(), location.getY(), location.getZ());
     }
 
+    public static Vector3F createFloatVectorFromLocation(Location location) {
+        return createVectorFromLocation(location).toFloatVector();
+    }
+
     public static Location createLocationFromVector(World world, Vector3D vector) {
         return new Location(world, vector.getX(), vector.getY(), vector.getZ());
     }
 
+    public static Location createLocationFromVector(World world, Vector3F vector) {
+        return new Location(world, vector.getX(), vector.getY(), vector.getZ());
+    }
+
     public static Location createLocationFromVector(World world, Vector3D vector, float yaw, float pitch) {
+        return new Location(world, vector.getX(), vector.getY(), vector.getZ(), yaw, pitch);
+    }
+
+    public static Location createLocationFromVector(World world, Vector3F vector, float yaw, float pitch) {
         return new Location(world, vector.getX(), vector.getY(), vector.getZ(), yaw, pitch);
     }
 }
