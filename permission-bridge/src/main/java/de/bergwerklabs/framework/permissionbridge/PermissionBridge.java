f8087441940ac9ce1be92cb2f14bc74cff0f0dee
freggy <riegeryannic@gmail.com>
2018-02-27 01:10:15 +0100

    Add documentation

--- permission-bridge/src/main/java/de/bergwerklabs/framework/permissionbridge/PermissionBridge.java
+++ permission-bridge/src/main/java/de/bergwerklabs/framework/permissionbridge/PermissionBridge.java
@@ -1,20 +1,43 @@
 package de.bergwerklabs.framework.permissionbridge;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.UUID;
 
 /**
  * Created by Yannic Rieger on 27.02.2018.
  * <p>
+ * Interface for providing basic functionality, that will be provided by various permission systems.
  *
  * @author Yannic Rieger
  */
 public interface PermissionBridge {
 
-    int getGroupIdAsInt(UUID uuid);
+    /**
+     * Gets the group id as an int. This method is temporary.
+     *
+     * @param uuid {@link UUID} of the player.
+     */
+    int getGroupIdAsInt(@NotNull UUID uuid);
 
-    GroupId getGroupAsObject(UUID uuid);
+    /**
+     * A {@link GroupId} object.
+     *
+     * @param uuid {@link UUID} of the player.
+     */
+    GroupId getGroupAsObject(@NotNull UUID uuid);
 
-    String getGroupPrefix(UUID uuid);
+    /**
+     * Gets the group prefix.
+     *
+     * @param uuid {@link UUID} of the player.
+     */
+    String getGroupPrefix(@NotNull UUID uuid);
 
-    String getGroupSuffix(UUID uuid);
+    /**
+     * Gets the group suffix.
+     *
+     * @param uuid {@link UUID} of the player.
+     */
+    String getGroupSuffix(@NotNull UUID uuid);
 }
